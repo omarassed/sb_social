0c808e36b6ef779b0a9229b668122ee8f48f9c3e
Omar <omar.assed@gmail.com>
2026-01-28 06:53:16 -0700

    establish key between socialprofile and socialuser

--- src/main/java/com/social/media/models/SocialUser.java
+++ src/main/java/com/social/media/models/SocialUser.java
@@ -1,15 +1,14 @@
 package com.social.media.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class SocialUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String userName;
 
+    @OneToOne(mappedBy = "user")
+//    @JoinColumn(name = "social_profile_id")
+    private SocialProfile socialProfile;
 }
